bfc87d6a12c2203c1c052ee73450622695af8af3
Kulbhushan saxena <kulbhushan.saxena@thomsonreuters.com>
2020-07-01 14:20:27 +0530

    Week2 : DS

---
+++ src/main/java/week2/ArrayManipulation.java
@@ -0,0 +1,63 @@
+package week2;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayManipulation {
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries, int m) {
+        long arr[] = new long[n+1];
+        
+        for(int i=0; i<m; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            int value = queries[i][2];
+            
+            while(start<=end) { 
+                arr[start] = arr[start] + value;
+                start++;
+            }
+            
+        }
+        
+        return Arrays.stream(arr).max().getAsLong();
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
